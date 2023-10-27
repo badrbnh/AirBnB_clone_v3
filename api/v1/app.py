f9f76599073c47d63fd75231660505a9f0a5d6a0
Halmouus <javierminimaz@gmail.com>
2023-10-27 22:27:54 +0100

    docstrings + pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,16 +9,19 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """handles teardown appcontext"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """handler for 404 errors"""
     return make_response(jsonify({'error': 'Not found'}), 404)
-    
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
