22186b72f9605e267a8b21daae2f0dc200dfc607
badrbnh <badrechahide@gmail.com>
2023-10-28 22:48:11 +0100

    requierments .0

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,16 +3,11 @@
 new view for User objects that handles all
 default RESTFul API actions
 """
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
-from flask import jsonify, request
+
 from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
+from models.user import User
 
 
 @app_views.route('/users',
