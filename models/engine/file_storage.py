71e878cd90a08f2ac4841f43eb1afb75a4f210ba
Halmouus <javierminimaz@gmail.com>
2023-10-28 18:45:33 +0100

    add get and count methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID,
+        or None if not found"""
+        if cls is None or id is None:
+            return None
+        return self.__objects[f"{cls.__name__}.{id}"]
+
+    def count(self, cls=None):
+        """count files in storage"""
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
